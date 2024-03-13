a01c0aa75368f3d708e6885e939e45085fa6d191
Pengyu Nie <prodigy.sov@gmail.com>
2024-03-13 08:53:22 -0400

    finished version of the strategy design pattern

--- pattern-strategy/src/main/java/ca/uwaterloo/cs446/designpatterns/strategy/RandomPercentDiscount.java
+++ pattern-strategy/src/main/java/ca/uwaterloo/cs446/designpatterns/strategy/RandomPercentDiscount.java
@@ -4,14 +4,17 @@ package ca.uwaterloo.cs446.designpatterns.strategy;
  * A discount strategy that reduces the full price by a percentage randomly selected within a range.
  */
 public class RandomPercentDiscount implements Discount {
+    private double minPercent;
+    private double maxPercent;
+
     public RandomPercentDiscount(double minPercent, double maxPercent) {
-        // TODO: implement
-        throw new UnsupportedOperationException("Not implemented");
+        this.minPercent = minPercent;
+        this.maxPercent = maxPercent;
     }
 
     @Override
     public double compute(double fullPrice) {
-        // TODO: implement
-        throw new UnsupportedOperationException("Not implemented");
+        double percent = this.minPercent + Math.random() * (this.maxPercent - this.minPercent);
+        return fullPrice * (1 - percent / 100);
     }
 }
